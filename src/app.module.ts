436ac31ab05b4a41fac145942c9d3deef72ce96c
Pritesh <priteshpmantri@gmail.com>
2021-11-17 23:53:19 +0530

    Switched to postgre

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ require("dotenv").config();
 @Module({
 	imports: [
 		TypeOrmModule.forRoot({
-			type: "mysql",
+			type: "postgres",
 			host: "localhost",
-			port: 3306,
+			port: 5432,
 			username: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
+			// password: process.env.DB_PASSWORD,
 			database: process.env.DB_NAME,
 			entities: [User],
 			synchronize: true,
