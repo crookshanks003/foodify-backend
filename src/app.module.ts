2893428e13afcd19f41354336d22f5b3c0ad4079
Pritesh <priteshpmantri@gmail.com>
2021-11-17 23:53:19 +0530

    Switched to postgre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
 import { Module } from "@nestjs/common";
 import { Connection } from "typeorm";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "./entities/"
-import { AuthModule } from './modules/auth/auth.module';
+import { User } from "./entities/";
+import { AuthModule } from "./modules/auth/auth.module";
 require("dotenv").config();
 
 @Module({
 	imports: [
 		TypeOrmModule.forRoot({
-			type: "mysql",
-			host: "localhost",
-			port: 3306,
+			type: "postgres",
+			host: process.env.DB_HOST,
+			port: 5432,
 			username: process.env.DB_USER,
 			password: process.env.DB_PASSWORD,
 			database: process.env.DB_NAME,
 			entities: [User],
 			synchronize: true,
+			ssl: {rejectUnauthorized:false},
 		}),
 		AuthModule,
 	],
